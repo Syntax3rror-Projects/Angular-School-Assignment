cac05121c77c6cfae84a37b0bec7ccfd5e14c60d
MauricioLudwig <mrdamiengallagher@gmail.com>
2017-11-22 11:22:06 +0100

    Routing

--- syntax3rror-app/src/app/components/navbar/navbar.component.ts
+++ syntax3rror-app/src/app/components/navbar/navbar.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  SiteTitle = 'Good Films';
+  SiteTitle = 'Syntax3rror';
 
   constructor() { }
 
