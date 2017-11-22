d860528d86546e2be9c682a82451b0801c3fcdb2
Jimmy Waern <jimmy.waern@hotmail.com>
2017-11-22 13:12:08 +0100

    Request Form

--- syntax3rror-app/src/app/app.module.ts
+++ syntax3rror-app/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
+import { RequestFormComponent } from './components/request-form/request-form.component';
 
 const appRoutes = [
   { path: '', component: HomeComponent},
@@ -19,11 +21,13 @@ const appRoutes = [
     NavbarComponent,
     AdminComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    RequestFormComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
