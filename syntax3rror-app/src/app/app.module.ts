ee866b3829addbf82315d394446c2b0e685cec1c
MauricioLudwig <mrdamiengallagher@gmail.com>
2017-11-22 12:36:06 +0100

    About component added

--- syntax3rror-app/src/app/app.module.ts
+++ syntax3rror-app/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
 
 const appRoutes = [
   { path: '', component: HomeComponent},
@@ -19,7 +20,8 @@ const appRoutes = [
     NavbarComponent,
     AdminComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
