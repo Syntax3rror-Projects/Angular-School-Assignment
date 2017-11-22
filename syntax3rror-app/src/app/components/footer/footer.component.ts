db3dbff88689e57c9873de30072d0fd0610e3c5a
Tommy Forsberg <tommy.forsberg@hotmail.com>
2017-11-22 13:52:07 +0100

    foot job

--- syntax3rror-app/src/app/components/footer/footer.component.ts
+++ syntax3rror-app/src/app/components/footer/footer.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+  adressFields: string[] = ['Syntax3rror', 'Adressvägen 18', '128 95 Stockholm'];
   constructor() { }
 
   ngOnInit() {
