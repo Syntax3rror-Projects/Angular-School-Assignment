8a267e25d83491d71ca2a0a3001239abe078da55
Tommy Forsberg <tommy.forsberg@hotmail.com>
2017-11-22 11:46:01 +0100

    nav bar update

--- syntax3rror-app/src/app/components/navbar/navbar.component.ts
+++ syntax3rror-app/src/app/components/navbar/navbar.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  SiteTitle = 'Syntax3rror';
+  SiteTitle = 'Syntax3rror Job Request';
 
   constructor() { }
 
