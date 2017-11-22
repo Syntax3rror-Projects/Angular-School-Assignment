009a1c783173297432fc0b5a42e97150539703f2
Jimmy Waern <jimmy.waern@hotmail.com>
2017-11-22 14:16:00 +0100

    Request Form

--- syntax3rror-app/src/app/app.module.ts
+++ syntax3rror-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule, Routes } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AngularFontAwesomeModule} from 'angular-font-awesome';
@@ -33,6 +34,7 @@ const appRoutes = [
   ],
   imports: [
     BrowserModule,
+    NgbModule,
     RouterModule.forRoot(appRoutes),
     ReactiveFormsModule,
     AngularFontAwesomeModule,
