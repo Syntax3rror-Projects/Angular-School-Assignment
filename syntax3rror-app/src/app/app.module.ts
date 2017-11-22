ed376ae0ca37797b80abf0e362350f55204f5ece
MauricioLudwig <mrdamiengallagher@gmail.com>
2017-11-22 13:31:59 +0100

    Added comma

--- syntax3rror-app/src/app/app.module.ts
+++ syntax3rror-app/src/app/app.module.ts
@@ -8,11 +8,8 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
-<<<<<<< HEAD
 import { AboutComponent } from './components/about/about.component';
-=======
 import { RequestFormComponent } from './components/request-form/request-form.component';
->>>>>>> d860528d86546e2be9c682a82451b0801c3fcdb2
 
 const appRoutes = [
   { path: '', component: HomeComponent },
@@ -27,11 +24,8 @@ const appRoutes = [
     AdminComponent,
     FooterComponent,
     HomeComponent,
-<<<<<<< HEAD
-    AboutComponent
-=======
+    AboutComponent,
     RequestFormComponent
->>>>>>> d860528d86546e2be9c682a82451b0801c3fcdb2
   ],
   imports: [
     BrowserModule,
