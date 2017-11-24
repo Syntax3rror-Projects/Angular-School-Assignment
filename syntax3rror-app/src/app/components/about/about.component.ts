d340d8e8ca7576fa9821daf223d3b1c9d41ba112
MauricioLudwig <mrdamiengallagher@gmail.com>
2017-11-24 12:32:38 +0100

    Image changed

--- syntax3rror-app/src/app/components/about/about.component.ts
+++ syntax3rror-app/src/app/components/about/about.component.ts
@@ -8,11 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
 
   cssClass: string;
+  imageUrl: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.imageUrl = 'https://www.mathsisfun.com/numbers/images/fibonacci.jpg';
     this.cssClass = 'online';
+    setTimeout(() => {
+      this.imageUrl = 'https://apod.nasa.gov/apod/image/mona_lisa.gif';
+    }, 3000);
     setTimeout(() => {
       this.cssClass = 'offline';
     }, 3000);
